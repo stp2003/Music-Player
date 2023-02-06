4178b9f250887c8edd65230cff9c6339ed7e5b83
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-02-06 18:45:12 +0530

    listView made and showing audio from device

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,88 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:melody_mate/constants/colors.dart';
+import 'package:melody_mate/controller/player_controller.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: bgDarkColor,
-        appBar: AppBar(
-          backgroundColor: bgDarkColor,
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.search,
-                color: whiteColor,
-              ),
-            ),
-          ],
-          leading: const Icon(
-            Icons.sort_rounded,
-            color: whiteColor,
-          ),
-          title: const Text(
-            'Melody Mate',
-            style: TextStyle(
-              fontFamily: 'poppins_bold',
-              fontSize: 19.5,
-              color: whiteColor,
-              letterSpacing: 1.2,
-            ),
-          ),
-          centerTitle: true,
-        ),
+    var controller = Get.put(PlayerController());
 
-        //??
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            itemCount: 100,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                margin: const EdgeInsets.only(bottom: 8.0),
-                child: ListTile(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                  ),
-                  tileColor: bgColor,
-                  title: const Text(
-                    'Music Name',
-                    style: TextStyle(
-                      fontFamily: 'poppins_bold',
-                      fontSize: 16.5,
-                      color: whiteColor,
-                      letterSpacing: 1.2,
-                    ),
-                  ),
-                  subtitle: const Text(
-                    'Artist Name',
-                    style: TextStyle(
-                      fontFamily: 'poppins',
-                      fontSize: 12.5,
-                      color: whiteColor,
-                      letterSpacing: 0.8,
-                    ),
-                  ),
-                  leading: const Icon(
-                    Icons.music_note,
-                    color: whiteColor,
-                    size: 32.0,
-                  ),
-                  trailing: const Icon(
-                    Icons.play_arrow_sharp,
-                    color: whiteColor,
-                    size: 26.0,
-                  ),
-                ),
-              );
-            },
+    return Scaffold(
+      backgroundColor: bgDarkColor,
+      appBar: AppBar(
+        backgroundColor: bgDarkColor,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              color: whiteColor,
+            ),
+          ),
+        ],
+        leading: const Icon(
+          Icons.sort_rounded,
+          color: whiteColor,
+        ),
+        title: const Text(
+          'Melody Mate',
+          style: TextStyle(
+            fontFamily: 'poppins_bold',
+            fontSize: 19.5,
+            color: whiteColor,
+            letterSpacing: 1.2,
           ),
         ),
+        centerTitle: true,
+      ),
+
+      //??
+      body: FutureBuilder<List<SongModel>>(
+        future: controller.audioQuery.querySongs(
+          ignoreCase: true,
+          orderType: OrderType.ASC_OR_SMALLER,
+          sortType: null,
+          uriType: UriType.EXTERNAL,
+        ),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.data == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text(
+                'No songs found!',
+              ),
+            );
+          } else {
+            return Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemCount: snapshot.data?.length,
+                // itemCount: 100,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 12.0),
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                      tileColor: bgColor,
+                      title: Text(
+                        snapshot.data![index].displayNameWOExt,
+                        style: const TextStyle(
+                          fontFamily: 'poppins_bold',
+                          fontSize: 16.5,
+                          color: whiteColor,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '${snapshot.data![index].artist}',
+                        style: const TextStyle(
+                          fontFamily: 'poppins',
+                          fontSize: 12.5,
+                          color: whiteColor,
+                          letterSpacing: 0.8,
+                        ),
+                      ),
+                      leading: const Icon(
+                        Icons.music_note,
+                        color: whiteColor,
+                        size: 32.0,
+                      ),
+                      trailing: const Icon(
+                        Icons.play_arrow_sharp,
+                        color: whiteColor,
+                        size: 26.0,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+        },
       ),
     );
   }
